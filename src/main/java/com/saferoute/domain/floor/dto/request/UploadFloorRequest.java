43b223f2d485c93242fa934a05fcb1b216f65e0d
seonghyeon choi <koukai@kakao.com>
2026-08-04 20:59:47 +0900

    Add validation for realWidth and realHeight fields

--- src/main/java/com/saferoute/domain/floor/dto/request/UploadFloorRequest.java
+++ src/main/java/com/saferoute/domain/floor/dto/request/UploadFloorRequest.java
@@ -5,8 +5,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 public record UploadFloorRequest(
     @NotNull Integer floorNum,
-    @NotNull Double realWidth,
-    @NotNull Double realHeight,
+    @Positive @NotNull Double realWidth,
+    @Positive @NotNull Double realHeight,
     @NotNull MultipartFile file
 ) {
 
