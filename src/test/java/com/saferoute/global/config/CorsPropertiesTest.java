2b2b49e5c70448014d50eda7f3e8cf1a2f08dcf2
songmin0111 <violet2111@naver.com>
2026-08-09 06:45:07 +0900

    [FIX]

--- src/test/java/com/saferoute/global/config/CorsPropertiesTest.java
+++ src/test/java/com/saferoute/global/config/CorsPropertiesTest.java
@@ -17,13 +17,10 @@ class CorsPropertiesTest {
                 "https://ds-saferoute.site"
         ));
 
-        assertEquals(
-                List.of(
-                        "http://localhost:3000",
-                        "https://ds-saferoute.site"
-                ),
-                properties.allowedOrigins()
-        );
+        assertEquals(List.of(
+                "http://localhost:3000",
+                "https://ds-saferoute.site"
+        ), properties.allowedOrigins());
     }
 
     @Test
@@ -40,12 +37,52 @@ class CorsPropertiesTest {
     void rejectsBlankAllowedOrigin() {
         assertThrows(
                 IllegalStateException.class,
-                () -> new CorsProperties(
-                        List.of(
-                                "http://localhost:3000",
-                                " "
-                        )
-                )
+                () -> new CorsProperties(List.of("http://localhost:3000", " "))
+        );
+    }
+
+    @Test
+    @DisplayName("wildcard Origin은 설정 오류로 처리한다")
+    void rejectsWildcardOrigin() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> new CorsProperties(List.of("*"))
+        );
+    }
+
+    @Test
+    @DisplayName("경로가 포함된 Origin은 설정 오류로 처리한다")
+    void rejectsOriginWithPath() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> new CorsProperties(List.of("https://ds-saferoute.site/api"))
+        );
+    }
+
+    @Test
+    @DisplayName("쿼리가 포함된 Origin은 설정 오류로 처리한다")
+    void rejectsOriginWithQuery() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> new CorsProperties(List.of("https://ds-saferoute.site?source=test"))
+        );
+    }
+
+    @Test
+    @DisplayName("host가 없는 Origin은 설정 오류로 처리한다")
+    void rejectsOriginWithoutHost() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> new CorsProperties(List.of("https:///missing-host"))
+        );
+    }
+
+    @Test
+    @DisplayName("http 또는 https가 아닌 Origin은 설정 오류로 처리한다")
+    void rejectsUnsupportedScheme() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> new CorsProperties(List.of("ftp://ds-saferoute.site"))
         );
     }
 }
