cca1bfdfb45c5f05baf9199b0af569ad13d8c456
PHJ <qkrguswl1021@duksung.ac.kr>
2026-08-24 02:03:26 +0900

    feat(s3): 이미지 조회용 Presigned GET URL 발급 추가
    S3PresignedUrlService에 createGetUrl을 추가한다. 업로드용(PUT, 1분)과
    별도로 조회용 만료 시간(presigned-get-url-expiration, 기본 5분)을 두어
    관리자 화면이 이미지를 불러오는 동안 만료되지 않게 한다.

--- src/main/java/com/saferoute/infrastructure/s3/service/S3PresignedUrlService.java
+++ src/main/java/com/saferoute/infrastructure/s3/service/S3PresignedUrlService.java
@@ -3,12 +3,16 @@ package com.saferoute.infrastructure.s3.service;
 import com.saferoute.global.api.error.S3ErrorCode;
 import com.saferoute.global.api.exception.ApiException;
 import com.saferoute.infrastructure.s3.config.S3Properties;
+import com.saferoute.infrastructure.s3.dto.PresignedGetUrl;
 import com.saferoute.infrastructure.s3.dto.PresignedPutUrl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
@@ -35,6 +39,30 @@ public class S3PresignedUrlService {
             PresignedPutObjectRequest presignedRequest =
                     s3Presigner.presignPutObject(presignRequest);
             return new PresignedPutUrl(
+                    presignedRequest.url().toString(),
+                    presignedRequest.expiration()
+            );
+        } catch (SdkException exception) {
+            throw new ApiException(S3ErrorCode.PRESIGNED_URL_GENERATION_FAILED, exception);
+        }
+    }
+
+    // 관리자 화면이 S3 object key만으로는 이미지를 열람할 수 없어(버킷은 Public이 아님) 조회용으로 발급한다.
+    public PresignedGetUrl createGetUrl(String objectKey) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(s3Properties.bucket())
+                .key(objectKey)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(s3Properties.presignedGetUrlExpiration())
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        try {
+            PresignedGetObjectRequest presignedRequest =
+                    s3Presigner.presignGetObject(presignRequest);
+            return new PresignedGetUrl(
                     presignedRequest.url().toString(),
                     presignedRequest.expiration()
             );
