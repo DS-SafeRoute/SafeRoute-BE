e5fb9ce951336ca77a6fad43f08bd9511cf63c64
seonghyeon choi <koukai@kakao.com>
2026-08-30 18:15:25 +0900

    Change participantCount validation to @Positive

--- src/main/java/com/saferoute/domain/report/dto/GenerateReportRequest.java
+++ src/main/java/com/saferoute/domain/report/dto/GenerateReportRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.PositiveOrZero;
 
 // 훈련 종료 후 관리자가 입력하는 값.
 public record GenerateReportRequest(
-        @NotNull @PositiveOrZero Integer participantCount,
+        @NotNull @Positive Integer participantCount,
         @NotNull @PositiveOrZero Integer survivorCount
 ) {
 }
